74c4968b5c49e15d3e5ff5badfbe300142cd3d2e
Sofia <Perlari>
2020-05-14 11:31:09 +0200

    prima parte milestone 3: cambio conversazione con click su contatto

--- main.js
+++ main.js
@@ -58,6 +58,40 @@ $('#contacts-filter').keyup(function() {
     }
 });
 
+// intercetto il click su un contatto
+$('.contact').click(function() {
+
+    // tolgo la classe active a tutti i div .contact
+    $('.contact').removeClass('active');
+    // aggiungo la classe active al contatto su cui ho cliccato
+    $(this).addClass('active');
+
+    // recupero il valore dell'attributo data-chat del contatto su cui ho cliccato
+    // var chat = $(this).data('chat');
+    var chat = $(this).attr('data-chat');
+    // console.log('data-chat letto dal contatto :' + chat);
+
+    // tolgo la classe active a tutti i div right-messages per nascondere tutti i pannelli delle chat
+    $('.right-messages').removeClass('active');
+    // recupero il div right-messages che ha lo stesso attributo data-chat del contatto su cui ho cliccato e ci assegno la classe active
+    $('.right-messages[data-chat="' + chat + '"]').addClass('active');
+
+    // recupero il nome del contatto su cui ho cliccato
+    var nome_contatto = $(this).find('.contact-name').text();
+    // console.log(nome_contatto);
+    // inserisco il nome del contatto nella parte di intestazione di destra
+    $('#header-right-contact-name').text(nome_contatto);
+
+    // recupero il percorso del file dell'immagine del contatto su cui ho cliccato
+    var src_contatto = $(this).find('.contact-logo img').attr('src');
+    // console.log(src_contatto);
+    // imposto il percorso del file dell'immagine nella parte di intestazione di destra
+    $('.header-right-logo img').attr('src', src_contatto);
+
+});
+
+
+
 // funzione per inviare un nuovo messaggio
 function invia_messaggio() {
     // recupero il testo inserito dall'utente nell'input
@@ -87,7 +121,7 @@ function risposta_pc() {
     // aggiungo la classe "received" al messaggio
     nuovo_messaggio_pc.addClass('received');
     // inserisco il testo "ok" nello span "message-text"
-    nuovo_messaggio_pc.children('.message-text').text('ok');
+    nuovo_messaggio_pc.children('.message-text').html('ciao<br>come stai?');
     // inserisco il nuovo messaggio nel contenitore di tutti i messaggi della conversazione
     $('.right-messages.active').append(nuovo_messaggio_pc);
 }
